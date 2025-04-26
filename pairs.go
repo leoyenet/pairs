e0d515981cf50f076cc69a86e2415c96ce4c42ca
leoyenet <voglarleon@gmail.com>
2025-04-26 21:03:27 +0200

    inital commit

---
+++ pairs.go
@@ -0,0 +1,28 @@
+package pairs
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Pair[K, V comparable] struct {
+	First  K
+	Second V
+}
+
+func New[K, V comparable](p1 K, p2 V) *Pair[K, V] {
+	return &Pair[K, V]{p1, p2}
+}
+
+func (p *Pair[K, V]) String() string {
+	return fmt.Sprintf("(%v, %v)", p.First, p.Second)
+}
+
+func (p *Pair[K, V]) Equal(p2 *Pair[K, V]) bool {
+	return p.First == p2.First && p.Second == p2.Second
+}
+
+func Sort[K, V comparable](slice []Pair[K, V], less func(i, j int) bool) []Pair[K, V] {
+	sort.Slice(slice, less)
+	return slice
+}
---
+++ pairs_test.go
@@ -0,0 +1,33 @@
+package pairs
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEqual(t *testing.T) {
+	first := Pair[int, int]{1, 2}
+	second := Pair[int, int]{1, 2}
+	assert.True(t, first.Equal(&second) && second.Equal(&first), "it should be equal")
+}
+
+func TestString(t *testing.T) {
+	p := Pair[int, int]{1, 2}
+	assert.Equal(t, p.String(), "(1, 2)", "result should be (1, 2)")
+}
+
+func TestSort(t *testing.T) {
+	p := []Pair[int, int]{{1, 10}, {2, 9}, {4, 5}}
+
+	t.Run("low to high", func(t *testing.T) {
+		assert.Equal(t, []Pair[int, int]{{4, 5}, {2, 9}, {1, 10}}, Sort(p, func(i, j int) bool {
+			return p[i].Second < p[j].Second
+		}))
+	})
+
+	t.Run("high to low", func(t *testing.T) {
+		assert.Equal(t, []Pair[int, int]{{1, 10}, {2, 9}, {4, 5}}, Sort(p, func(i, j int) bool {
+			return p[i].Second > p[j].Second
+		}))
+	})
+}
